c7392e4487af2e4aeb2127a67f9d5e8f0d6ec337
Oladapo Fadeyi <ofadeyi@gmail.com>
2023-03-03 14:22:44 +0000

    NG-T-317 | v0.0.6

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let moduleName = "YotraKit"
 let gitlabProjectId = "44017035"
-let version = "0.0.5"
+let version = "0.0.6"
 let url = "https://gitlab.com/api/v4/projects/\(gitlabProjectId)/packages/generic/yotrakit/\(version)/\(moduleName).zip"
-let checksum = "1b450d641898af35e5ada8fd1b5a04fdead1286ba45dde5d1e193835eb41eeec"
+let checksum = "424d57696c7a0a4067452aee5d75ae5f18b2df535dfeefac5ac241c3d669dbe5"
 
 let package = Package(
         name: moduleName,
