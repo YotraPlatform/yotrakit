6aae8e8a3244c5ffd510e7c3f084ce8d47ce82c8
Oladapo Fadeyi <ofadeyi@gmail.com>
2023-03-03 14:13:21 +0000

    NG-T-317 | Remove swift-log as a dependency

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let moduleName = "YotraKit"
 let gitlabProjectId = "44017035"
-let version = "0.0.3"
+let version = "0.0.4"
 let url = "https://gitlab.com/api/v4/projects/\(gitlabProjectId)/packages/generic/yotrakit/\(version)/\(moduleName).zip"
 let checksum = "e54c417f216ae609621bb48fbbd8584ce980cf2b51c8b808d970d1e881ec8a39"
 
@@ -16,9 +16,6 @@ let package = Package(
             .library(
                     name: moduleName,
                     targets: [moduleName]),
-        ],
-        dependencies: [
-            .package(url: "https://github.com/apple/swift-log", from: "1.5.2"),
         ],
         targets: [
             .binaryTarget(
