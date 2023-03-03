0792f380dd6fd34c6c6f81854380a5220d5d533d
Oladapo Fadeyi <ofadeyi@gmail.com>
2023-03-03 12:50:06 +0000

    NG-T-317 | Add swift-log a a dependency

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 
 let moduleName = "YotraKit"
 let gitlabProjectId = "44017035"
-let version = "0.0.1"
+let version = "0.0.2"
 let url = "https://gitlab.com/api/v4/projects/\(gitlabProjectId)/packages/generic/yotrakit/\(version)/\(moduleName).zip"
-let checksum = "12d4302d045c42f6fb7f637b0f32b4c6cbc7ca18b93de904c38eb0f5a168cf91"
+let checksum = "6a3cedb0a85cfafe9fb8051d48b3376645b2513962c2d4526442a8f77c1c5696"
 
 let package = Package(
         name: moduleName,
@@ -16,6 +16,9 @@ let package = Package(
             .library(
                     name: moduleName,
                     targets: [moduleName]),
+        ],
+        dependencies: [
+            .package(url: "https://github.com/apple/swift-log", from: "1.5.2"),
         ],
         targets: [
             .binaryTarget(
