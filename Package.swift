5b4c4caa9c86383fd55a69f30b2023e362549cb5
Oladapo Fadeyi <ofadeyi@gmail.com>
2023-08-08 03:24:24 +0100

    NG-T-270 | v0.0.28 - DevUX improvements

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@
 import PackageDescription
 
 let moduleName = "YotraKit"
-let version = "0.0.27"
+let version = "0.0.28"
 let url = "https://raw.githubusercontent.com/YotraPlatform/yotrakit/main/artefacts/\(version)/\(moduleName).zip"
 let path = "artefacts/\(version)/\(moduleName).zip"
-let checksum = "c31faead0c9986ed90bb3beee93602a493b5d611eba5e221d88d1590c261b430"
+let checksum = "7e98f52a268357ba56a93e2671f8b7f166182c9205a8e4e1b01ced1c2e5fe7b2"
 let mode = "url"
 let binaryTarget = mode == "local" ?
         PackageDescription.Target.binaryTarget(name: moduleName, path: path) :
