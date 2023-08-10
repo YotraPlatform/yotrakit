d61be72d56b8a0a6e1e34a5eaa9ecf1015a7a0cc
Oladapo Fadeyi <ofadeyi@gmail.com>
2023-08-10 11:09:25 +0100

    NG-T-270 | v0.0.29 - AR session 20230810-01

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@
 import PackageDescription
 
 let moduleName = "YotraKit"
-let version = "0.0.28"
+let version = "0.0.29"
 let url = "https://raw.githubusercontent.com/YotraPlatform/yotrakit/main/artefacts/\(version)/\(moduleName).zip"
 let path = "artefacts/\(version)/\(moduleName).zip"
-let checksum = "7e98f52a268357ba56a93e2671f8b7f166182c9205a8e4e1b01ced1c2e5fe7b2"
+let checksum = "e74bf04d003d909eeffb655a82736e284e79fae66fa465e306cea237473febeb"
 let mode = "url"
 let binaryTarget = mode == "local" ?
         PackageDescription.Target.binaryTarget(name: moduleName, path: path) :
