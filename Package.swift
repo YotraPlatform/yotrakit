64476dcf846ddd09ed9b0873551d9a29979bd326
Oladapo Fadeyi <ofadeyi@gmail.com>
2024-01-22 01:12:54 +0000

    NG-T-270 | v0.0.39 - Enriched the definition of YKCard and YKTicket structs

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@
 import PackageDescription
 
 let moduleName = "YotraKit"
-let version = "0.0.38"
+let version = "0.0.39"
 let url = "https://raw.githubusercontent.com/YotraPlatform/yotrakit/main/artefacts/\(version)/\(moduleName).zip"
 let path = "artefacts/\(version)/\(moduleName).zip"
-let checksum = "29cff89a73df740555dc2ab5990d75001179688c7dcfcaeab19af7ea847e25b3"
+let checksum = "2f54b17eccf3521324c43aea4a2a1e24a473892b04cf194a9057462740f2b196"
 let mode = "url"
 let binaryTarget = mode == "local" ?
         PackageDescription.Target.binaryTarget(name: moduleName, path: path) :
