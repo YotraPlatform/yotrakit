3fccc8f5662bbf23d4ebbb55fb5ccb62c836f6ff
Oladapo Fadeyi <ofadeyi@gmail.com>
2024-01-26 14:53:08 +0000

    NG-T-270 | v0.0.40 - Random card.location selection fix

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@
 import PackageDescription
 
 let moduleName = "YotraKit"
-let version = "0.0.39"
+let version = "0.0.40"
 let url = "https://raw.githubusercontent.com/YotraPlatform/yotrakit/main/artefacts/\(version)/\(moduleName).zip"
 let path = "artefacts/\(version)/\(moduleName).zip"
-let checksum = "2f54b17eccf3521324c43aea4a2a1e24a473892b04cf194a9057462740f2b196"
+let checksum = "3dabc76f008ca4282d6faededc3330c1e3b88cafc47bc75bf0bfbcc57510de5f"
 let mode = "url"
 let binaryTarget = mode == "local" ?
         PackageDescription.Target.binaryTarget(name: moduleName, path: path) :
