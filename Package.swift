3b020ea297d943c427d07c7c0db19002ad734df4
Oladapo Fadeyi <ofadeyi@gmail.com>
2024-04-19 09:20:00 +0100

    NG-T-270 | v0.0.48 - Implement delete-ticket

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@
 import PackageDescription
 
 let moduleName = "YotraKit"
-let version = "0.0.47"
+let version = "0.0.48"
 let url = "https://raw.githubusercontent.com/YotraPlatform/yotrakit/main/artefacts/\(version)/\(moduleName).zip"
 let path = "artefacts/\(version)/\(moduleName).zip"
-let checksum = "a5397865ca01a65c78926a9cff080e25110585a7d3fa7bb465983c53efcb11b0"
+let checksum = "2ebcf9218c64a57f11c337e97d2e3c1194dd961f5d91da63feb05d816a58c019"
 let mode = "url"
 let binaryTarget = mode == "local" ?
         PackageDescription.Target.binaryTarget(name: moduleName, path: path) :
