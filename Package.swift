6ef82eae6d59e71ded4da5c0595bbf782f706e1f
Oladapo Fadeyi <ofadeyi@gmail.com>
2025-08-28 07:21:03 +0100

    NG-T-270 | v0.0.61 - Minor Dev UX updates — Update struct definitions to improve serialisation/deserialisation

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@
 import PackageDescription
 
 let moduleName = "YotraKit"
-let version = "0.0.60"
+let version = "0.0.61"
 let url = "https://raw.githubusercontent.com/YotraPlatform/yotrakit/main/artefacts/\(version)/\(moduleName).zip"
 let path = "artefacts/\(version)/\(moduleName).zip"
-let checksum = "77e273a2c32d5b0af3d7d177ebc5365598ea4f0210378b55551f3d8dc34667cd"
+let checksum = "3615e06a8ad51e5c944c8520f5a9f951b4b0b4880eb3efc85f9a3ebb779faf69"
 let mode = "url"
 let binaryTarget = mode == "local" ?
         PackageDescription.Target.binaryTarget(name: moduleName, path: path) :
