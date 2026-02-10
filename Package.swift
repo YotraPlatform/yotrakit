b87f50da0f201f8acde1861ffe868f4395230094
Oladapo Fadeyi <ofadeyi@gmail.com>
2026-02-10 11:40:02 +0000

    NG-T-270 | v0.0.62 - Dev UX updates — Exposed functions to allow the testing of the SDK authentication flows

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@
 import PackageDescription
 
 let moduleName = "YotraKit"
-let version = "0.0.61"
+let version = "0.0.62"
 let url = "https://raw.githubusercontent.com/YotraPlatform/yotrakit/main/artefacts/\(version)/\(moduleName).zip"
 let path = "artefacts/\(version)/\(moduleName).zip"
-let checksum = "3615e06a8ad51e5c944c8520f5a9f951b4b0b4880eb3efc85f9a3ebb779faf69"
+let checksum = "05194f93e601f28329dc58f761b26c50d42321ec0399dde44fba0b7954f784e0"
 let mode = "url"
 let binaryTarget = mode == "local" ?
         PackageDescription.Target.binaryTarget(name: moduleName, path: path) :
